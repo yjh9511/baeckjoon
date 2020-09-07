afdb4bdd164bd1f181c7fc5c5ea5c5ab79a153c3
yjh9511 <yjh6845@naver.com>
2020-09-07 20:37:14 +0900

    Create 2193(dp).py

---
+++ practice/2193(dp).py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin=open("input.txt","r")
+
+n=int(input())
+
+d=[[0]*2 for _ in range(n+1)]
+
+d[1][1]=1
+
+for i in range(2,n+1):
+    d[i][0]=d[i-1][0]+d[i-1][1]
+    d[i][1]=d[i-1][0]
+
+print(d[n][0]+d[n][1])
+
+        
+
